9ce4065bd6dcded906b528ee9255fb1ed138df67
LuuLong <luulong6786@gmail.com>
2021-12-23 14:05:18 +0700

    commit-3

--- src/StaffList/StaffListComponent.js
+++ src/StaffList/StaffListComponent.js
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import {
   Card,
   CardImg,
+  Form,
   Col,
   Input,
   FormGroup,
@@ -14,109 +15,247 @@ import {
 } from "reactstrap";
 import { Link } from "react-router-dom";
 import { STAFFS } from "../shared/staffs";
+import { DEPARTMENTS } from "../shared/staffs";
 
-//Tạo hàm hiển thị AllList nhân viên
-function RenderAllStaffList({ x }) {
-  return (
-    <Card>
-      {/* Khi click vào sẽ dẫn đến URL: */}
-      <Link to={`/nhanvien/${x.id}`}>
-        <CardImg width="100%" src={x.image} />
-        <p>{x.name}</p>
-      </Link>
-    </Card>
-  );
-}
+class StaffListComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isModalOpen: false,
+      searchValue: "",
+      id: parseInt([STAFFS.length]),
+      name: "",
+      doB: "",
+      startDate: "",
+      salaryScale: "",
+      department: "",
+      annualLeave: "",
+      overTime: "",
+      salary: "",
+      image: "/assets/images/alberto.png",
+      touched: {
+        name: false,
+        doB: false,
+        startDate: false,
+      },
+    };
+    this.toggleModal = this.toggleModal.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
+    this.validate = this.validate.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleBlur = this.handleBlur.bind(this);
+    this.handleAddNewStaff = this.handleAddNewStaff.bind(this);
+  }
+  toggleModal() {
+    this.setState({ isModalOpen: !this.state.isModalOpen });
+  }
+  //Tạo method get value từ input của search, sau đó cập nhật lại state ban đầu
+  handleSearch(e) {
+    this.setState({ searchValue: e.target.value });
+  }
+  handleBlur = (field) => (e) => {
+    this.setState({ touched: { ...this.state.touched, [field]: true } });
+  };
+  //Tạo method get value của input và thay đổi state/ dựa trên name của input (gửi giá trị của thuộc tính trong form về server)
+  handleInputChange(e) {
+    const value = e.target.value;
+    const name = e.target.name;
+    this.setState({ [name]: value });
+  }
+  handleAddNewStaff(e) {
+    const newStaff = {
+      id: parseInt(this.state.id + 1),
+      name: this.state.name,
+      doB: this.state.doB,
+      salaryScale: this.state.salaryScale,
+      startDate: this.state.startDate,
+      department: this.state.department,
+      annualLeave: this.state.annualLeave,
+      overTime: this.state.overTime,
+      salary: this.state.salaryScale * 3000000 + this.state.overTime * 200000,
+      image: this.state.image,
+    };
+    this.toggleModal();
+    return STAFFS.push(newStaff), alert(JSON.stringify(newStaff));
+  }
+  //Tạo method validate với chức năng render
+  validate(name, doB, startDate) {
+    const errors = { name: "", doB: "", startDate: "" };
+    //Validate name//
+    if (this.state.touched.name && name.length < 2)
+      errors.name = "Yêu cầu nhập nhiều hơn 2 ký tự";
+    else if (this.state.touched.name && name.length > 30)
+      errors.name = "Yêu cầu nhập ít hơn 30 ký tự";
+    //Validate doB//
+    if (this.state.touched.doB && doB === "") errors.doB = "Yêu cầu nhập";
+    else if (new Date(doB) > new Date())
+      errors.doB = "Nhập lại năm sinh cho đúng";
+    //Validate startDate
+    if (this.state.touched.startDate && startDate === "")
+      errors.startDate = "Yêu cầu nhập";
+    else if (new Date(startDate) > new Date())
+      errors.startDate = "Thông tin nhập vào chưa hợp lệ";
 
-//Tạo FunctionComponent để render list nhân viên//
-function StaffListComponent() {
-  const [isModalOpen, setModalOpen] = useState(false);
-  const [searchValue, setSearchValue] = useState("");
+    return errors;
+  }
 
-  // Lọc nhân viên theo tên
-  const filteredStaff = STAFFS.filter(
-    (staff) =>
-      staff.name.toLowerCase().indexOf(searchValue.toLowerCase()) !== -1
-  );
-
-  // Sau khi lọc xong thì hiển thị list nhân viên lọc lên màn hình
-  const stafflist = filteredStaff.map((name) => {
+  render() {
+    //Render 1 staff cụ thể
+    const RenderAllStaffList = ({ x }) => {
+      return (
+        <Card>
+          {/* Khi click vào sẽ dẫn đến URL: */}
+          <Link to={`/nhanvien/${x.id}`}>
+            <CardImg width="100%" src={x.image} />
+            <p>{x.name}</p>
+          </Link>
+        </Card>
+      );
+    };
+    //Lấy data từ searchInput ->lọc qua từng mảng
+    const filterStaff = STAFFS.filter((staff) => {
+      return (
+        staff.name
+          .toLowerCase()
+          .indexOf(this.state.searchValue.toLowerCase()) !== -1
+      );
+    });
+    const stafflist = filterStaff.map((name) => {
+      return (
+        <div className="col-6 col-md-4 col-lg-2">
+          <RenderAllStaffList x={name} />
+        </div>
+      );
+    });
+    const errors = this.validate(
+      this.state.name,
+      this.state.doB,
+      this.state.startDate
+    );
     return (
-      <div className="col-6 col-md-4 col-lg-2">
-        <RenderAllStaffList x={name} />
+      <div className="container">
+        <div className="row">
+          <FormGroup row className="col-12 col-md-6">
+            <h4>Nhân viên</h4>
+            <button
+              type="submit"
+              value="submit"
+              className="btn btn"
+              onClick={this.toggleModal}
+            >
+              <i class="fa fa-plus-square-o fa-2x" aria-hidden="true"></i>
+            </button>
+          </FormGroup>
+          <FormGroup row className="col-12 col-md-6">
+            <Col>
+              <Input
+                label="Search Name"
+                icon="search"
+                placeholder="Search"
+                onChange={this.handleSearch}
+              ></Input>
+            </Col>
+          </FormGroup>
+        </div>
+        {/* Tạo modal */}
+
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+          <ModalHeader>Thêm nhân viên</ModalHeader>
+          <ModalBody>
+            <Form onSubmit={this.handleAddNewStaff}>
+              <FormGroup>
+                <Label>Tên</Label>
+                <Input
+                  placeholder="Nhập tên đầy đủ"
+                  type="text"
+                  name="name"
+                  onBlur={this.handleBlur("name")}
+                  onChange={this.handleInputChange}
+                  value={this.state.name}
+                  valid={errors.name}
+                  invalid={errors.name !== ""}
+                ></Input>
+                <FormFeedback>{errors.name}</FormFeedback>
+              </FormGroup>
+              <FormGroup>
+                <Label>Ngày sinh</Label>
+                <Input
+                  type="date"
+                  name="doB"
+                  value={this.state.doB}
+                  onChange={this.handleInputChange}
+                  onBlur={this.handleBlur("doB")}
+                  valid={errors.doB}
+                  invalid={errors.doB !== ""}
+                ></Input>
+                <FormFeedback>{errors.doB}</FormFeedback>
+              </FormGroup>
+              <FormGroup>
+                <Label>Ngày vào công ty</Label>
+                <Input
+                  type="date"
+                  name="startDate"
+                  value={this.state.startDate}
+                  onChange={this.handleInputChange}
+                  onBlur={this.handleBlur("startDate")}
+                  valid={errors.startDate}
+                  invalid={errors.startDate !== ""}
+                ></Input>
+                <FormFeedback>{errors.startDate}</FormFeedback>
+              </FormGroup>
+              <FormGroup>
+                <Label>Phòng Ban</Label>
+                <Input
+                  type="select"
+                  name="department"
+                  onChange={this.handleInputChange}
+                >
+                  <option value="" disabled selected>
+                    Chọn phòng ban
+                  </option>
+                  <option value={DEPARTMENTS[0].name}>Sale</option>
+                  <option value={DEPARTMENTS[1].name}>HR</option>
+                  <option value={DEPARTMENTS[2].name}>Marketing</option>
+                  <option value={DEPARTMENTS[3].name}>IT</option>
+                  <option value={DEPARTMENTS[4].name}>Finance</option>
+                </Input>
+              </FormGroup>
+              <FormGroup>
+                <Label>Hệ số lương</Label>
+                <Input
+                  type="number"
+                  name="salary"
+                  onChange={this.handleInputChange}
+                ></Input>
+              </FormGroup>
+              <FormGroup>
+                <Label>Số ngày nghỉ còn lại</Label>
+                <Input
+                  type="number"
+                  name="annualLeave"
+                  onChange={this.handleInputChange}
+                ></Input>
+              </FormGroup>
+              <FormGroup>
+                <Label>Số ngày đã làm thêm</Label>
+                <Input
+                  type="number"
+                  name="overTime"
+                  onChange={this.handleInputChange}
+                ></Input>
+              </FormGroup>
+
+              <Button type="submit" color="primary">
+                Thêm
+              </Button>
+            </Form>
+          </ModalBody>
+        </Modal>
+
+        <div className="row ">{stafflist}</div>
       </div>
     );
-  });
-  return (
-    <div className="container">
-      <div className="row">
-        <FormGroup row className="col-12 col-md-6">
-          <h4>Nhân viên</h4>
-          <button
-            type="submit"
-            value="submit"
-            className="btn btn"
-            onClick={() => setModalOpen(!isModalOpen)}
-          >
-            <i class="fa fa-plus-square-o fa-2x" aria-hidden="true"></i>
-          </button>
-        </FormGroup>
-        <FormGroup row className="col-12 col-md-6">
-          <Col>
-            <Input
-              label="Search Name"
-              icon="search"
-              placeholder="Search"
-              onChange={(e) => setSearchValue(e.target.value)}
-            ></Input>
-          </Col>
-        </FormGroup>
-      </div>
-
-      <Modal isOpen={isModalOpen} toggle={() => setModalOpen(!isModalOpen)}>
-        <ModalHeader toggle={() => setModalOpen(!isModalOpen)}>
-          Thêm nhân viên
-        </ModalHeader>
-        <ModalBody>
-          <FormGroup>
-            <Label>Tên</Label>
-            <Input type="text" name="name"></Input>
-            <FormFeedback>{}</FormFeedback>
-          </FormGroup>
-          <FormGroup>
-            <Label>Ngày sinh</Label>
-            <Input type="text" name="doB"></Input>
-            <FormFeedback>{}</FormFeedback>
-          </FormGroup>
-          <FormGroup>
-            <Label>Ngày vào công ty</Label>
-            <Input type="text name" name="startDate"></Input>
-            <FormFeedback>{}</FormFeedback>
-          </FormGroup>
-          <FormGroup>
-            <Label>Phòng Ban</Label>
-            <Input type="text name" name="department"></Input>
-          </FormGroup>
-          <FormGroup>
-            <Label>Hệ số lương</Label>
-            <Input type="text name" name="salary"></Input>
-          </FormGroup>
-          <FormGroup>
-            <Label>Số ngày nghỉ còn lại</Label>
-            <Input type="text name" name="annualLeave"></Input>
-          </FormGroup>
-          <FormGroup>
-            <Label>Số ngày đã làm thêm</Label>
-            <Input type="text name" name="overTime"></Input>
-          </FormGroup>
-          <Button type="submit" color="primary">
-            Thêm
-          </Button>
-        </ModalBody>
-      </Modal>
-      <div className="row ">{stafflist}</div>
-    </div>
-  );
+  }
 }
 
 export default StaffListComponent;
